e5e863e99afd96577912aae3e8a5054d839325af
apryamostanov <anton.pryamostanov@gmail.com>
2020-05-26 12:17:32 +0300

    Progress

--- david-sdk/src/main/groovy/io/infinite/david/ascend/authentications/EmailOtpPreparator.groovy
+++ david-sdk/src/main/groovy/io/infinite/david/ascend/authentications/EmailOtpPreparator.groovy
@@ -45,11 +45,11 @@ class EmailOtpPreparator implements AuthenticationPreparator {
         Thread.sleep(1000)
         davidThread.silentSender.send("Please enter your email:", davidThread.chatId)
         String email = davidThread.waitForInput(30)
-        if (!email.matches(emailRegex)) {
+        if (!email.contains("@")) {
             davidThread.silentSender.send("Sorry, it seems like a wrong email, please try again:", davidThread.chatId)
             email = davidThread.waitForInput(30)
         }
-        if (!email.matches(emailRegex)) {
+        if (!email.contains("@")) {
             throw new DavidUserException("Sorry, it seems like a wrong email.")
         }
         def managedOtpHandle
@@ -64,7 +64,7 @@ class EmailOtpPreparator implements AuthenticationPreparator {
                                     "Authorization": "Bearer ${prerequisiteJwt.get()}"
                             ],
                             body: """{
-	"email": "+$email",
+	"to": "+$email",
 	"templateValues": {
 		"action": "Registration"
 	},
@@ -113,7 +113,5 @@ class EmailOtpPreparator implements AuthenticationPreparator {
                 )
         ).status
     }
-
-    String emailRegex = """(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"""
 
 }
